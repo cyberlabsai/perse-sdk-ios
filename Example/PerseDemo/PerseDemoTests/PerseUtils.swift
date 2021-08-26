eee3a9c0a519bd43420ca243c57bc3e0df586cc6
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2021-08-26 15:04:34 -0300

    build: update perse lite dependency

--- Example/PerseDemo/PerseDemoTests/PerseUtils.swift
+++ Example/PerseDemo/PerseDemoTests/PerseUtils.swift
@@ -7,18 +7,18 @@ public func detectWithFile(
     _ xctest: XCTestCase,
     imageName: String,
     apiKey: String,
-    onSuccess: @escaping (DetectResponse) -> Void,
+    onSuccess: @escaping (PerseAPIResponse.Face.Detect) -> Void,
     onError: @escaping (String) -> Void
 ) {
     let expectation = XCTestExpectation(description: "Perse face detect in a image.")
-    
+
     // Create a temp file image path from a resource.
     guard let tempFilePath: String = getTempFilePath(name: imageName) else {
         onError(Perse.Error.INVALID_IMAGE_PATH)
         expectation.fulfill()
         return
     }
-            
+
     // Start the face detect process.
     Perse(apiKey: apiKey, baseUrl: Environment.baseUrl)
         .face
@@ -34,7 +34,7 @@ public func detectWithFile(
         expectation.fulfill()
         return
     }
-    
+
     // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
     xctest.wait(for: [expectation], timeout: 10.0)
 }
@@ -43,18 +43,18 @@ public func detectWithData(
     _ xctest: XCTestCase,
     imageName: String,
     apiKey: String,
-    onSuccess: @escaping (DetectResponse) -> Void,
+    onSuccess: @escaping (PerseAPIResponse.Face.Detect) -> Void,
     onError: @escaping (String) -> Void
 ) {
     let expectation = XCTestExpectation(description: "Perse face detect in a image.")
-    
+
     // Get a temp data from a resource.
     guard let data: Data = getTempData(name: imageName) else {
         onError(Perse.Error.INVALID_IMAGE_PATH)
         expectation.fulfill()
         return
     }
-            
+    
     // Start the face detect process.
     Perse(apiKey: apiKey, baseUrl: Environment.baseUrl)
         .face
@@ -70,7 +70,7 @@ public func detectWithData(
         expectation.fulfill()
         return
     }
-    
+
     // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
     xctest.wait(for: [expectation], timeout: 10.0)
 }
@@ -79,26 +79,26 @@ public func compareWithFile(
     _ xctest: XCTestCase,
     firstImageName: String,
     secondImageName: String,
-    apiKey: String,    
-    onSuccess: @escaping (CompareResponse) -> Void,
+    apiKey: String,
+    onSuccess: @escaping (PerseAPIResponse.Face.Compare) -> Void,
     onError: @escaping (String) -> Void
 ) {
     let expectation = XCTestExpectation(description: "Perse face compare.")
-    
+
     // Create a temp file image path from a resource.
     guard let firstTempFilePath: String = getTempFilePath(name: firstImageName) else {
         onError(Perse.Error.INVALID_IMAGE_PATH)
         expectation.fulfill()
         return
     }
-    
+
     // Create a temp file image path from a resource.
     guard let secondTempFilePath: String = getTempFilePath(name: secondImageName) else {
         onError(Perse.Error.INVALID_IMAGE_PATH)
         expectation.fulfill()
         return
     }
-            
+
     // Start the face detect process.
     Perse(apiKey: apiKey, baseUrl: Environment.baseUrl)
         .face
@@ -116,7 +116,7 @@ public func compareWithFile(
         expectation.fulfill()
         return
     }
-    
+
     // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
     xctest.wait(for: [expectation], timeout: 10.0)
 }
@@ -126,25 +126,25 @@ public func compareWithData(
     firstImageName: String,
     secondImageName: String,
     apiKey: String,
-    onSuccess: @escaping (CompareResponse) -> Void,
+    onSuccess: @escaping (PerseAPIResponse.Face.Compare) -> Void,
     onError: @escaping (String) -> Void
 ) {
     let expectation = XCTestExpectation(description: "Perse face compare.")
-    
+
     // Get data from file image path from a resource.
     guard let firstData: Data = getTempData(name: firstImageName) else {
         onError(Perse.Error.INVALID_IMAGE_PATH)
         expectation.fulfill()
         return
     }
-    
+
     // Get data from file image path from a resource.
     guard let secondData: Data = getTempData(name: secondImageName) else {
         onError(Perse.Error.INVALID_IMAGE_PATH)
         expectation.fulfill()
         return
     }
-            
+
     // Start the face detect process.
     Perse(apiKey: apiKey, baseUrl: Environment.baseUrl)
         .face
@@ -162,7 +162,109 @@ public func compareWithData(
         expectation.fulfill()
         return
     }
+
+    // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+    xctest.wait(for: [expectation], timeout: 10.0)
+}
+
+public func faceCreate(
+    _ xctest: XCTestCase,
+    imageName: String,
+    apiKey: String,
+    onSuccess: @escaping (PerseAPIResponse.Enrollment.Face.Create) -> Void,
+    onError: @escaping (String) -> Void
+) {
+    let expectation = XCTestExpectation(description: "Perse face create.")
+
+    // Get a temp data from a resource.
+    guard let data: Data = getTempData(name: imageName) else {
+        onError(Perse.Error.INVALID_IMAGE_PATH)
+        expectation.fulfill()
+        return
+    }
     
+    // Start the face detect process.
+    Perse(
+        apiKey: apiKey,
+        baseUrl: Environment.baseUrl
+    )
+        .face
+        .enrollment
+        .create(data)
+    {
+        response in
+        onSuccess(response)
+        expectation.fulfill()
+        return
+    } onError: {
+        status, error in
+        onError(status)
+        expectation.fulfill()
+        return
+    }
+
+    // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+    xctest.wait(for: [expectation], timeout: 10.0)
+}
+
+public func faceRead(
+    _ xctest: XCTestCase,
+    apiKey: String,
+    onSuccess: @escaping (PerseAPIResponse.Enrollment.Face.Read) -> Void,
+    onError: @escaping (String) -> Void
+) {
+    let expectation = XCTestExpectation(description: "Perse face read.")
+
+    // Start the face detect process.
+    Perse(apiKey: apiKey, baseUrl: Environment.baseUrl)
+        .face
+        .enrollment
+        .read()
+    {
+        response in
+        onSuccess(response)
+        expectation.fulfill()
+        return
+    } onError: {
+        status, error in
+        onError(status)
+        expectation.fulfill()
+        return
+    }
+
+    // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+    xctest.wait(for: [expectation], timeout: 10.0)
+}
+
+public func faceDelete(
+    _ xctest: XCTestCase,
+    userToken: String,
+    apiKey: String,
+    onSuccess: @escaping (PerseAPIResponse.Enrollment.Face.Delete) -> Void,
+    onError: @escaping (String) -> Void
+) {
+    let expectation = XCTestExpectation(description: "Perse face delete.")
+    
+    // Start the face detect process.
+    Perse(
+        apiKey: apiKey,
+        baseUrl: Environment.baseUrl
+    )
+        .face
+        .enrollment
+        .delete(userToken)
+    {
+        response in
+        onSuccess(response)
+        expectation.fulfill()
+        return
+    } onError: {
+        status, error in
+        onError(status)
+        expectation.fulfill()
+        return
+    }
+
     // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
     xctest.wait(for: [expectation], timeout: 10.0)
 }
